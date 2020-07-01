2f0d7fcd785ecd91046c8fa5b2806ae183ff55e2
Nikita Kalinnikov <niki9888985@mail.ru>
2020-07-01 08:55:41 +0300

    добавит возможность прыгать

---
+++ Assets/Scripts/Player/Jumping.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class Jumping: CachedComponents
+{
+    /// <summary>
+    /// Сила прыжка объекта.
+    /// </summary>
+    [SerializeField, Range(0, 1000)]
+    float JumpForce = 1.0f;
+
+#if UNITY_EDITOR
+    [SerializeField]
+    KeyCode JumpKey = KeyCode.Space;
+#endif
+    private void Update()
+    {
+#if UNITY_EDITOR
+        if (Input.GetKeyDown(JumpKey))
+        {
+            Jump();
+        }
+#else
+         if (Input.touchCount > 0)
+        {
+            Jump();
+        }
+#endif
+    }
+
+    /// <summary>
+    /// Приложит силу к объекту вертикально вверх.
+    /// </summary>
+    void Jump()
+    {
+        cachedRigidbody.AddForce(Vector2.up * JumpForce, ForceMode2D.Impulse);
+    }
+}
