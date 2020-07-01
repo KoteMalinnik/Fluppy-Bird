c3f01d5350ac44c5e97790e39d479769683ff006
Nikita Kalinnikov <niki9888985@mail.ru>
2020-07-01 10:13:19 +0300

    добавит Obstacles и изменение размера его дырки

--- Assets/Scripts/Obstacles/HoleResizer.cs
+++ Assets/Scripts/Obstacles/HoleResizer.cs
@@ -1,18 +1,32 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
+/// <summary>
+/// Изменение размера дырки объекта Obstacles в режиме Edit.
+/// </summary>
 public class HoleResizer : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    Transform upperBox = null;
+
+    [SerializeField]
+    Transform downerBox = null;
+
+    [SerializeField]
+    BoxCollider2D hole = null;
+
+    [Space, SerializeField, Range(1,5)]
+    float holeSize = 2.0f;
+
+    private void OnValidate()
     {
-        
+        resize();
     }
 
-    // Update is called once per frame
-    void Update()
+    [ExecuteInEditMode]
+    void resize()
     {
-        
+        hole.size = new Vector2(1, holeSize);
+        upperBox.transform.localPosition = new Vector3(0, 5 + holeSize/2, 0);
+        downerBox.transform.localPosition = -upperBox.transform.localPosition;
     }
 }
