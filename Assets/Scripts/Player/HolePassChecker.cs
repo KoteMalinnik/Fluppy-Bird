974494c3b059e04a61dd66422a9f3adc8530bd44
Nikita Kalinnikov <niki9888985@mail.ru>
2020-07-01 10:18:50 +0300

    добавит проверку на прохождение дырки Obstacle

---
+++ Assets/Scripts/Player/HolePassChecker.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HolePassChecker: MonoBehaviour
+{
+    [ExecuteInEditMode]
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        Log.Message("Успешное прохождение дырки.");
+    }
+}
