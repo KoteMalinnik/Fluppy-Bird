c773af6532be0718bf99d7470e618dade2f3d1e2
Nikita Kalinnikov <niki9888985@mail.ru>
2020-07-01 09:54:56 +0300

    добавит слежение камеры за объектом и папку для скриптов препятствий

---
+++ Assets/Scripts/CameraFolowing.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class CameraFolowing : CachedComponent<Transform>
+{
+    /// <summary>
+    /// Объект для слежения.
+    /// </summary>
+    [SerializeField]
+    Transform targetTransform = null;
+
+    /// <summary>
+    /// Отступ по горизонтальной оси;
+    /// </summary>
+    [SerializeField]
+    float offsetX = -4;
+
+   
+    private void Awake()
+    {
+        if (targetTransform == null)
+        {
+            targetTransform = GameObject.FindWithTag("Player").transform;
+        }
+    }
+
+    private void Update()
+    {
+        Component.position = new Vector3(targetTransform.position.x + offsetX, 0, -10);
+    }
+}
