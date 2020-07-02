367cfb9c7f7e35e9df0e64bd7c525dd65ae44587
Nikita Kalinnikov <niki9888985@mail.ru>
2020-07-02 17:34:38 +0300

    добавит оставшиеся скрипты и обновит VS

---
+++ Assets/Scripts/GUIController.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+
+public class MainMenuGUI : MonoBehaviour
+{
+    
+}
--- Assets/Scripts/StaticClasses/Serialization.cs
+++ Assets/Scripts/StaticClasses/Serialization.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MainMenuGUI : MonoBehaviour
+public class Serialization : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
