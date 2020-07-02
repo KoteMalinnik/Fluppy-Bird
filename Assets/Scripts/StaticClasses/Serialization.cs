c29004534cde1f09cac60db54e2e3b3bd0b0dd99
Nikita Kalinnikov <niki9888985@mail.ru>
2020-07-02 18:03:48 +0300

    реализует сереализацию данных int и bool

--- Assets/Scripts/StaticClasses/Serialization.cs
+++ Assets/Scripts/StaticClasses/Serialization.cs
@@ -1,18 +1,63 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class Serialization : MonoBehaviour
+/// <summary>
+/// Сериализация игровых данных.
+/// </summary>
+public static class Serialization
 {
-    // Start is called before the first frame update
-    void Start()
+    /// <summary>
+    /// Сохранение значения типа bool.
+    /// </summary>
+    /// <param name="key">Ключ.</param>
+    /// <param name="value">Значение.</param>
+    public static void Save(string key, bool value)
     {
-        
+        int val = value ? 1 : 0;
+        Save(key, val);
     }
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// Сохранение значения типа int.
+    /// </summary>
+    /// <param name="key">Ключ.</param>
+    /// <param name="value">Значение.</param>
+    public static void Save(string key, int value)
     {
-        
+        Log.Message($"Сохранения значения -{key}- = -{value}-.");
+        PlayerPrefs.SetInt(key, value);
+    }
+
+    /// <summary>
+    /// Загрузка значения типа int.
+    /// </summary>
+    /// <param name="key">Ключ.</param>
+    /// <param name="defaultValue">Значение по умолчанию.</param>
+    public static int Load(string key, int defaultValue = 0)
+    {
+        int value = defaultValue;
+        if (PlayerPrefs.HasKey(key)) value = PlayerPrefs.GetInt(key);
+       
+        Log.Message($"Загрузка значения -{key}- = -{value}-.");
+
+        return value;
+    }
+
+    /// <summary>
+    /// Загрузка значения типа bool.
+    /// </summary>
+    /// <param name="key">Ключ.</param>
+    /// <param name="defaultValue">Значение по умолчанию.</param>
+    public static bool Load(string key, bool defaultValue = true)
+    {
+        bool value = defaultValue;
+        if (PlayerPrefs.HasKey(key))
+        {
+            int val = PlayerPrefs.GetInt(key);
+            value = val == 1;
+        }
+       
+        Log.Message($"Загрузка значения -{key}- = -{value}-.");
+       
+        return value;
     }
 }
