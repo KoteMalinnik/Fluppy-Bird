f9367d68e19319890d07804c52f32512b6625429
Nikita Kalinnikov <niki9888985@mail.ru>
2020-07-05 14:34:23 +0300

    добавит Debug отображение вектора скорости

---
+++ Assets/Scripts/Player/RotationToVelocityVector.cs
@@ -0,0 +1,22 @@
+﻿ using UnityEngine;
+
+public class RotationToVelocityVector: CachedComponent<Rigidbody2D>
+{
+    Transform _transform = null;
+  
+    new Transform transform
+    {
+        get
+        {
+            if (_transform == null) _transform = GetComponent<Transform>();
+            return _transform;
+        }
+    }
+
+    Vector2 position = Vector2.zero;
+    private void Update()
+    {
+        position = transform.position;
+        Debug.DrawLine(position, position + Component.velocity);
+    }
+}
